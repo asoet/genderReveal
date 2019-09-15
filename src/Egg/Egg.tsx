81e69814dcf6fcf7162fa8e905c3af8174f482cb
Allard Soeters <asoeters@gericall.nl>
2019-09-15 17:52:05 +0200

    init

---
+++ src/Egg/Egg.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+import './Egg.css';
+
+import Full_Egg from './Images/FullEgg.png';
+import crack_1 from './Images/Crack 1.png';
+import crack_2 from './Images/Crack 2.png';
+import crack_3 from './Images/Crack 3.png';
+import crack_4 from './Images/Crack 4.png';
+import crack_5 from './Images/Crack 5.png';
+import crack_6 from './Images/Crack 6.png';
+import crack_7 from './Images/Crack 7.png';
+import Left_Half from './Images/Left Half.png';
+import Right_Half from './Images/Right Half.png';
+import Boy from './Images/boy.png';
+import Girl from './Images/girl.png';
+import SubTitle from './SubTitle';
+
+
+const Egg: React.FC = () => {
+  const [src, setSrc] = useState(Full_Egg);
+  const [count, setCount] = useState(0);
+  const isBoy = false;
+
+  const click = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    switch(count){
+        case 0: setSrc(crack_1);
+        break;
+        case 3: setSrc(crack_2);
+        break;
+        case 5: setSrc(crack_3);
+        break;
+        case 7: setSrc(crack_4);
+        break;
+        case 9: setSrc(crack_5);
+        break;
+        case 11: setSrc(crack_6);
+        break;
+        case 13: setSrc(crack_7);
+        break;
+        case 15: setSrc(isBoy? Boy: Girl);
+        break;
+    }
+    setCount(count + 1);
+
+  }
+
+
+  let className = "Egg";
+  if(count >= 16){
+    className = "Egg-boygirl";
+  }
+  const max = 16;
+
+  return (
+      <div onClick={click} className="Page-container">
+      {count >= max && <>
+        <img src={Left_Half} className="Egg-left" alt="Egg" />
+        </>}
+    <div   className="Container">
+        <img  src={src} className={className} alt="Egg" />
+    </div>
+    
+    {count >= max && <>
+        <img src={Right_Half} className="Egg-right" alt="Egg" />
+        </>}
+      <SubTitle count={count} max={max} isBoy={isBoy} />
+    </div>
+  );
+}
+
+export default Egg;
---
+++ src/Egg/SubTitle.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import './Egg.css';
+
+type SubTitleProps = {
+    count: number;
+    max: number
+    isBoy: boolean
+}
+
+const SubTitle: React.FC<SubTitleProps> = ({count, max, isBoy}) => {
+
+  return (<>
+    {(count % 2 !== 0) && count < max && <p className="boy-text">Is het een jongen?</p> }
+    {(count % 2 === 0) && count < max && <p className="girl-text">Is het een meisje?</p> }
+    {count >= max && isBoy && <p className="boy-text">Een Jongen</p>}
+    {count >= max && !isBoy && <p className="girl-text">Een meisje</p>}
+    </>
+  );
+}
+
+export default SubTitle;
